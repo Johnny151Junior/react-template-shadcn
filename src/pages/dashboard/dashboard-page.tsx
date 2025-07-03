a6fbccd8e73f901dfebd889d106f36b33200602a
Johnny Xaiyalath <147270448+Johnny151Junior@users.noreply.github.com>
2025-07-03 16:01:36 +0700

    finished template  (2)

--- src/pages/dashboard/dashboard-page.tsx
+++ src/pages/dashboard/dashboard-page.tsx
@@ -1,17 +1,5 @@
-import { Button } from "@/components/ui/button";
-import { getMe } from "@/services/auth/auth-service";
-import { Link } from "react-router";
-
 function DashboardPage() {
-  const firstTime = async () => {
-    await Promise.all([getMe(), getMe()]);
-  };
-  return (
-    <>
-      Dashbaord<Button onClick={firstTime}></Button>
-      <Link to="/page-2">goooo</Link>
-    </>
-  );
+  return <>HELLO</>;
 }
 
 export default DashboardPage;
